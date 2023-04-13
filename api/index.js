28d6977eac82ef9773efeb23fd9c6514f7b68c60
Sandeep-Balli <sandeepballi.tech@gmail.com>
2023-04-13 16:09:35 +0530

    Error while displaying uploaded images

--- api/index.js
+++ api/index.js
@@ -7,7 +7,9 @@ const jwt = require('jsonwebtoken');
 const User = require('./models/User');
 const cookieParser = require('cookie-parser');
 const imageDownloader = require('image-downloader');
+const multer = require('multer');
 const dotenv = require('dotenv');
+const fs = require('fs');
 dotenv.config();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -88,6 +90,20 @@ app.post('/upload-by-link', async (req, res) => {
         dest: __dirname + '/uploads/' +newName
     })
     res.json(newName);
+})
+
+const photosMiddleware = multer({dest: 'uploads/'});
+app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
+    const uploadedFiles = [];
+    for(let i = 0; i < req.files.length; i++) {
+        const {path, originalname} = req.files[i];
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        const newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+        uploadedFiles.push(newPath.replace('uploads/',''));
+    }
+    res.json(uploadedFiles);
 })
 
 app.listen(4000);
