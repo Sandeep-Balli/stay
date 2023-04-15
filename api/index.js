31f8866a2123e734cf94276ecdfd11290211c7e2
Sandeep-Balli <sandeepballi.tech@gmail.com>
2023-04-15 19:39:35 +0530

    Create New Functionality work done

--- api/index.js
+++ api/index.js
@@ -5,11 +5,13 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User');
+const Place = require('./models/Place');
 const cookieParser = require('cookie-parser');
 const imageDownloader = require('image-downloader');
 const multer = require('multer');
 const dotenv = require('dotenv');
 const fs = require('fs');
+const path = require('path');
 dotenv.config();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -17,7 +19,7 @@ const jwtSecret = 'gdrsoiaauvmnryydaaaenegp'
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads', express.static(__dirname+'/uploads'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
     origin: 'http://127.0.0.1:5173'
@@ -44,19 +46,19 @@ app.post('/register', async (req, res) => {
 })
 
 app.post('/login', async (req, res) => {
-    const {email, password} = req.body;
-    const userDoc = await User.findOne({email});
-    if(userDoc) {
+    const { email, password } = req.body;
+    const userDoc = await User.findOne({ email });
+    if (userDoc) {
         const passOk = bcrypt.compareSync(password, userDoc.password);
-        if(passOk) {
+        if (passOk) {
             jwt.sign({
-                email: userDoc.email, 
+                email: userDoc.email,
                 id: userDoc._id
             }, jwtSecret, {}, (err, token) => {
-                if(err) throw err;
+                if (err) throw err;
                 res.cookie('token', token).json(userDoc);
             });
-            
+
         } else {
             res.status(422).json('password not ok');
         }
@@ -66,12 +68,12 @@ app.post('/login', async (req, res) => {
 })
 
 app.get('/profile', (req, res) => {
-    const {token} = req.cookies;
-    if(token) {
+    const { token } = req.cookies;
+    if (token) {
         jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-            if(err) throw err;
-            const {name, email, _id} = await User.findById(userData.id);
-            res.json({name, email, _id});
+            if (err) throw err;
+            const { name, email, _id } = await User.findById(userData.id);
+            res.json({ name, email, _id });
         })
     } else {
         res.json(null);
@@ -83,27 +85,96 @@ app.post('/logout', (req, res) => {
 })
 
 app.post('/upload-by-link', async (req, res) => {
-    const {link} = req.body;
+    const { link } = req.body;
     const newName = 'photo' + Date.now() + '.jpg';
     await imageDownloader.image({
         url: link,
-        dest: __dirname + '/uploads/' +newName
+        dest: __dirname + '/uploads/' + newName
     })
     res.json(newName);
 })
 
-const photosMiddleware = multer({dest: 'uploads/'});
-app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
-    const uploadedFiles = [];
-    for(let i = 0; i < req.files.length; i++) {
-        const {path, originalname} = req.files[i];
-        const parts = originalname.split('.');
-        const ext = parts[parts.length - 1];
-        const newPath = path + '.' + ext;
-        fs.renameSync(path, newPath);
-        uploadedFiles.push(newPath.replace('uploads/',''));
-    }
-    res.json(uploadedFiles);
+// const photosMiddleware = multer({dest: 'uploads/'});
+// app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
+//     const uploadedFiles = [];
+//     for(let i = 0; i < req.files.length; i++) {
+//         const {path, originalname} = req.files[i];
+//         const parts = originalname.split('.');
+//         const ext = parts[parts.length - 1];
+//         const newPath = path + '.' + ext;
+//         fs.renameSync(path, newPath);
+//         uploadedFiles.push(newPath.replace('uploads/',''));
+//     }
+//     res.json(uploadedFiles);
+// })
+
+const storage = multer.diskStorage({
+    destination: 'uploads/',
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+    },
+});
+
+const upload = multer({ storage });
+
+app.post('/upload', upload.array('photos', 100), (req, res) => {
+    res.json(req.files);
+});
+
+// const photosMiddleware = multer({dest: 'uploads/'});
+// app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
+//     res.json(req.files);
+// })
+
+app.post('/places', (req, res) => {
+    const { token } = req.cookies;
+    const { title, address, addedPhotos,
+        description, perks, extraInfo,
+        checkIn, checkOut, maxGuests } = req.body;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        if (err) throw err;
+        const placeDoc = await Place.create({
+            owner: userData.id,
+            title, address, photos: addedPhotos,
+            description, perks, extraInfo,
+            checkIn, checkOut, maxGuests
+        });
+        res.json(placeDoc);
+    })
+})
+
+app.get('/places', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        const { id } = userData;
+        res.json(await Place.find({ owner: id }));
+    })
+})
+
+app.get('/places/:id', async (req, res) => {
+    const { id } = req.params;
+    res.json(await Place.findById(id));
+})
+
+app.put('/places', async (req, res) => {
+    const { token } = req.cookies;
+    const {
+        id, title, address, addedPhotos,
+        description, perks, extraInfo,
+        checkIn, checkOut, maxGuests
+    } = req.body;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        const placeDoc = await Place.findById(id);
+        if (userData.id === placeDoc.owner.toString()) {
+            placeDoc.set({
+                title, address, photos: addedPhotos,
+                description, perks, extraInfo,
+                checkIn, checkOut, maxGuests
+            })
+            await placeDoc.save();
+            res.json('ok');
+        }
+    })
 })
 
 app.listen(4000);
