9850d634fa74be3f51cce1521de0b033e5a147ef
Sandeep-Balli <sandeepballi.tech@gmail.com>
2023-04-18 01:54:10 +0530

    Done & Dusted💥

--- api/index.js
+++ api/index.js
@@ -6,12 +6,14 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User');
 const Place = require('./models/Place');
+const Booking = require('./models/Booking');
 const cookieParser = require('cookie-parser');
 const imageDownloader = require('image-downloader');
 const multer = require('multer');
 const dotenv = require('dotenv');
 const fs = require('fs');
 const path = require('path');
+
 dotenv.config();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -26,6 +28,17 @@ app.use(cors({
 }));
 
 mongoose.connect(process.env.MONGO_URL);
+
+
+function getUserDataFromReq(req) {
+    return new Promise((resolve, reject) => {
+        jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+            if (err) throw err;
+            resolve(userData);
+        })
+    })
+}
+
 
 app.get('/test', (req, res) => {
     res.json('test ok');
@@ -179,6 +192,23 @@ app.put('/places', async (req, res) => {
 
 app.get('/places', async (req, res) => {
     res.json(await Place.find());
+});
+
+app.post('/bookings', async (req, res) => {
+    const userData = await getUserDataFromReq(req);
+    const { place, checkIn, checkOut, numberOfGuests, name, phone, price } = req.body;
+    Booking.create({
+        place, checkIn, checkOut, numberOfGuests, name, phone, price, user: userData.id
+    }).then((doc) => {
+        res.json(doc);
+    }).catch((err) => {
+        throw err;
+    })
+})
+
+app.get('/bookings', async (req, res) => {
+    const userData = await getUserDataFromReq(req);
+    res.json(await Booking.find({user: userData.id}).populate('place') );
 })
 
 app.listen(4000);
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import { Link } from "react-router-dom";
 
 export default function IndexPage() {
     const [places, setPlaces] = useState([]);
@@ -11,9 +11,9 @@ export default function IndexPage() {
         })
     }, [])
     return (
-        <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="mt-8 grid gap-x-6 gap-y-8 xs:grid-cols:1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {places.length > 0 && places.map(place => (
-                <div>
+                <Link to={'/place/'+place._id}>
                     <div className="bg-gray-500 mb-2 rounded-2xl flex">
                         {place.photos?.[0] && (
                             <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:4000/uploads/' + place.photos?.[0]} alt="" />
@@ -25,7 +25,7 @@ export default function IndexPage() {
                         <span className="font-bold">${place.price} per night</span>
                         
                     </div>
-                </div>
+                </Link>
             ))}
         </div>
     )
